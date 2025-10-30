8273c39a055f93013b0adc8e6ff28c1bea752e0c
Yomi <lanteng233@gmail.com>
2025-10-30 16:52:07 +0800

    dftp testcases updated

--- dftp-client/src/test/scala/link/rdcn/broadcasting/DftpClientProxy.scala
+++ dftp-client/src/test/scala/link/rdcn/broadcasting/DftpClientProxy.scala
@@ -7,11 +7,11 @@
 package link.rdcn.broadcasting
 
 import link.rdcn.client.{ClientUtils, DftpClient, UrlValidator}
-import link.rdcn.server.BlobTicket
+import link.rdcn.message.BlobTicket
 import link.rdcn.struct.{Blob, DFRef, StructType}
 import link.rdcn.user.Credentials
 import link.rdcn.util.CodecUtils
-import org.apache.arrow.flight.{FlightClient, Ticket}
+import org.apache.arrow.flight.Ticket
 
 import java.io.InputStream
 import scala.collection.JavaConverters.asScalaBufferConverter
@@ -19,7 +19,7 @@ import scala.collection.mutable
 
 class DftpClientProxy(host:String,port:Int) extends DftpClient(host, port) {
 
-  override def getStream(flightClient: FlightClient, ticket: Ticket): (StructType, Iterator[Seq[Any]]) = {
+  override def getStream(ticket: Ticket): (StructType, Iterator[Seq[Any]]) = {
     val flightStream = flightClient.getStream(ticket)
     val vectorSchemaRootReceived = flightStream.getRoot
     val schema = ClientUtils.arrowSchemaToStructType(vectorSchemaRootReceived.getSchema)
@@ -50,7 +50,7 @@ class DftpClientProxy(host:String,port:Int) extends DftpClient(host, port) {
                   val blob = new Blob {
                     println(s"[${System.currentTimeMillis()}] DEBUG: Server A - Blob created. Connection 2 (to Server B) is now established but LAZY.")
 //                                        promise = Promise[Unit]()
-                    val iter = getStream(flightClient, blobTicket)._2
+                    val iter = getStream(blobTicket)._2
                     val chunkIterator = iter.map(value => {
                       value.head match {
                         case v: Array[Byte] => v
@@ -64,13 +64,9 @@ class DftpClientProxy(host:String,port:Int) extends DftpClient(host, port) {
                       try consume(stream)
                       finally {
                         stream.close()
-//                                                if(promise != null)
-//                                                  promise.success(())
                       }
                     }
                   }
-//                                    if(promise != null)
-//                                      Await.result(promise.future, 100.second)
                   (vec.getName, blob)
                 }
               case _ => throw new UnsupportedOperationException(s"Unsupported vector type: ${vec.getClass}")
@@ -83,6 +79,31 @@ class DftpClientProxy(host:String,port:Int) extends DftpClient(host, port) {
     (schema, iter)
   }
 
+  private class IteratorInputStream(it: Iterator[Array[Byte]]) extends InputStream {
+    private var currentChunk: Array[Byte] = Array.emptyByteArray
+    private var index: Int = 0
+
+    /** 从流中读取一个字节 */
+    override def read(): Int = {
+      if (currentChunk == null) return -1
+      if (index >= currentChunk.length) {
+        if (it.hasNext) {
+          currentChunk = it.next()
+          index = 0
+          read()
+        } else {
+          currentChunk = null
+          -1
+        }
+      } else {
+        val b = currentChunk(index) & 0xff
+        index += 1
+        b
+      }
+    }
+
+
+  }
 }
 
 object DftpClientProxy {
